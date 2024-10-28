c35b0eaf44611af9e8a21775004d20d8c25c0bd2
nyursoo <abdymomunov.nurseit@icloud.com>
2024-10-28 17:09:36 +0600

    shop

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.scss'
 
 import Home from './Pages/Home/Home.jsx';
-import Shop from './Pages/Shop/Shop.js';
+import Shop from './Pages/Shop/Shop.jsx';
 import {Route,Routes } from 'react-router-dom';
 import Single from './Pages/Single/Single.js';
 import NotFound from './Pages/NotFound/NotFound.js';
 import Layout from './Layout/Layout.jsx';
-import About from './Pages/AboutUs/AboutUs.js'
+import About from './Pages/AboutUs/AboutUs.jsx'
 import Carrier from './Pages/Carrier/Carrier.js'
 import News from './Pages/News/News.js'
 import Partners from './Pages/Partners/Partners.js'
